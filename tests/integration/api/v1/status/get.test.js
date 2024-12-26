2383efa2b85a995493583d1f17bf42d528c0d909
JoaoBG57 <129676790+JoaoBG57@users.noreply.github.com>
2024-12-26 05:57:52 +0000

    modificado tests/integration/api/v1/status/get.test.js

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,6 @@
+const fetch = require("node-fetch");
+
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-});
+}, 10000); // Aumenta o tempo limite para 10 segundos
