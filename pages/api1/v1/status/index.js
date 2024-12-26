422d93a1da87950d3f0d8f992e40498469adfb92
JoaoBG57 <129676790+JoaoBG57@users.noreply.github.com>
2024-12-26 05:25:21 +0000

    adicionado os arquivos ´database.js´ e ´env´

--- pages/api1/v1/status/index.js
+++ pages/api1/v1/status/index.js
@@ -1,6 +1,15 @@
 import database from "../../../../infra/database.js";
 
-function status(request, response) {
-  response.status(200).json({ chave: "são acima da média" });
+async function status(request, response) {
+  try {
+    const result = await database.query("SELECT 1 + 1 as sum");
+    response
+      .status(200)
+      .json({ chave: "são acima da média", resultado: result.rows[0].sum });
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: "Internal Server Error" });
+  }
 }
+
 export default status;
