6e8a5f59b41296e06b957766cf0aacd7bde58f6d
JoaoBG57 <129676790+JoaoBG57@users.noreply.github.com>
2024-12-27 00:42:44 +0000

    correções de arquivos renomeados e ajustes de pastas

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,4 @@
-import fetch from "node-fetch";
-
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-}, 10000); // Aumenta o tempo limite para 10 segundos
+});
