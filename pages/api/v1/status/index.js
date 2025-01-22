6632c2bf82e7c8db05778ee7acbdff3c46777d23
JoaoBG57 <129676790+JoaoBG57@users.noreply.github.com>
2025-01-22 14:35:50 -0300

    Update index.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,18 @@
+import { createRouter } from "next-connect";
 import database from "infra/database";
+import controller from "infra/controller.js";
 
-async function status(request, response) {
+const router = createRouter();
+
+router.get(getHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function getHandler(request, response) {
   const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
-
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
   );
@@ -31,5 +38,3 @@ async function status(request, response) {
     },
   });
 }
-
-export default status;
