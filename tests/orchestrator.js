3c3996ef1d3e997f9c069a30ee1545d31b03f8a0
JoaoBG57 <129676790+JoaoBG57@users.noreply.github.com>
2025-03-28 19:33:28 +0000

    feat: add user list endpoint

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,4 +1,4 @@
-import { version as uuidVersion } from "uuid";
+/* import { version as uuidVersion } from "uuid";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
@@ -118,4 +118,42 @@ describe("POST /api/v1/users", () => {
       });
     });
   });
-});
+});*/
+import retry from "async-retry";
+import database from "infra/database.js";
+import migrator from "models/migrator.js";
+
+async function waitForAllServices() {
+  await waitForWebServer();
+
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+
+      if (response.status !== 200) {
+        throw Error();
+      }
+    }
+  }
+}
+
+async function clearDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+async function runPendingMigrations() {
+  await migrator.runPendingMigrations();
+}
+
+const orchestrator = {
+  waitForAllServices,
+  clearDatabase,
+  runPendingMigrations,
+};
+
+export default orchestrator;
