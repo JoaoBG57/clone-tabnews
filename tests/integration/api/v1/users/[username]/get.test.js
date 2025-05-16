99c3a0a5c9f5f4ce44fd1fc5aecc34c312c3ddcd
JoaoBG57 <129676790+JoaoBG57@users.noreply.github.com>
2025-05-16 10:05:25 -0300

    Update get.test.js

--- tests/integration/api/v1/users/[username]/get.test.js
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -37,43 +37,16 @@ describe("GET /api/v1/users/[username]", () => {
         username: "MesmoCase",
         email: "mesmo.case@curso.dev",
         password: "senha123",
+        password: response2Body.password,
         created_at: response2Body.created_at,
         updated_at: response2Body.updated_at,
       });
-
-      expect(uuidVersion(response2Body.id)).toBe(4);
-      expect(Date.parse(response2Body.created_at)).not.toBeNaN();
-      expect(Date.parse(response2Body.updated_at)).not.toBeNaN();
-    });
-
-    test("With case mismatch", async () => {
-      const response1 = await fetch("http://localhost:3000/api/v1/users", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: "CaseDiferente",
-          email: "case.diferente@curso.dev",
-          password: "senha123",
-        }),
-      });
-
-      expect(response1.status).toBe(201);
-
-      const response2 = await fetch(
-        "http://localhost:3000/api/v1/users/casediferente",
-      );
-
-      expect(response2.status).toBe(200);
-
-      const response2Body = await response2.json();
-
-      expect(response2Body).toEqual({
+      
         id: response2Body.id,
         username: "CaseDiferente",
         email: "case.diferente@curso.dev",
         password: "senha123",
+        password: response2Body.password,
         created_at: response2Body.created_at,
         updated_at: response2Body.updated_at,
       });
