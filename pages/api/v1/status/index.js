fbd47b8935d6d1afa397ae18b3efe6ec064e92a8
JoaoBG57 <129676790+JoaoBG57@users.noreply.github.com>
2026-02-15 21:14:08 -0300

    Fix formatting and add newline at end of file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -13,6 +13,7 @@ async function getHandler(request, response) {
 
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
   );
